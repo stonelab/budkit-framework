ae46182eb0496bba20cc1fbea71c326e75ba3c3d
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-05 12:55:54 +0100

    Removes View/Layout/Engine and View/Layout/Parse Adds View/Layout/Compiler

---
+++ src/Budkit/View/Layout/Compiler.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Budkit\View\Layout;
+
+use Budkit\View\Parser;
+
+class Compiler extends Parser{
+	
+	
+}
--- src/Budkit/View/Layout/Engine.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-namespace Budkit\View\Layout;
-
-interface Engine{
-	
-}
