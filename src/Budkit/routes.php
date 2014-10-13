a40d07e8914b946737e2904302fb14d7460af5d6
Livingstone Fultang <livingstonefultang@gmail.com>
2014-10-13 20:34:10 +0100

    Will now use package pre-and post-install hooks to setup routes in /applications/routes directory

--- src/Budkit/routes.php
+++ src/Budkit/routes.php
@@ -1,10 +1,27 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: livingstonefultang
- * Date: 13/10/2014
- * Time: 16:13
- */
 
-//echo "route file included";
-//die;
+//$app->route->add("/callme", "callme")
+//	->setValues(array(
+//		"action"=>function($response, $params = null)use($app){
+//
+//			//var_dump($params);
+//
+//			$response->setContentType("json");
+//
+//			return $response->addContent("{j:s,o:n}");
+//
+//		})
+//	);
+//
+////Grouping routes under a prefix;
+//Route::attach("/blog", "blog", function($route){
+//	$route->setTokens(array(
+//	   'id'		=> '\d+',
+//	   'format'	=>'(\.json|\.atom|\.html)'
+//	));
+//   //subroutes
+//   $route->add( '{format}','browse');
+//   $route->add('/{id}{format}', "read");
+//   $route->add('/{id}/edit{format}', "edit");
+//
+//});
